d6c9ad96650c7cfe2bdc4d6ac8fc7e414bfbbd3d
OrangeUnivers <118116610+OrangeUnivers@users.noreply.github.com>
2024-11-11 07:15:24 +0100

    Add files via upload

---
+++ normal.js
@@ -0,0 +1,96 @@
+const defaultProject = {
+    "extensions": [],
+    "meta": {
+        "agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+        "semver": "3.0.0",
+        "vm": "4.8.32"
+    },
+    "monitors": [],
+    "targets": [
+        {
+            "blocks": {},
+            "broadcasts": {},
+            "comments": {},
+            "costumes": [
+                {
+                    "assetId": "cd21514d0531fdffb22204e0ec5ed84a",
+                    "dataFormat": "svg",
+                    "md5ext": "cd21514d0531fdffb22204e0ec5ed84a.svg",
+                    "name": "backdrop1",
+                    "rotationCenterX": 240,
+                    "rotationCenterY": 180
+                }
+            ],
+            "currentCostume": 0,
+            "isStage": true,
+            "layerOrder": 0,
+            "lists": {},
+            "name": "Stage",
+            "sounds": [
+                {
+                    "assetId": "83a9787d4cb6f3b7632b4ddfebf74367",
+                    "dataFormat": "wav",
+                    "format": "",
+                    "md5ext": "83a9787d4cb6f3b7632b4ddfebf74367.wav",
+                    "name": "pop",
+                    "rate": 44100,
+                    "sampleCount": 1032
+                }
+            ],
+            "tempo": 60,
+            "textToSpeechLanguage": null,
+            "variables": {
+                "`jEk@4|i[#Fk?(8x)AV.-my variable": [
+                    "my variable",
+                    0
+                ]
+            },
+            "videoState": "on",
+            "videoTransparency": 50,
+            "volume": 100
+        },
+        {
+            "blocks": {
+            },
+            "broadcasts": {},
+            "comments": {},
+            "costumes": [
+                {
+                    "assetId": "a98722f50129470f829e89b795f3a005",
+                    "bitmapResolution": 1,
+                    "dataFormat": "svg",
+                    "md5ext": `a98722f50129470f829e89b795f3a005.svg`,
+                    "name": "costume1",
+                    "rotationCenterX": 48,
+                    "rotationCenterY": 50
+                }
+            ],
+            "currentCostume": 0,
+            "direction": 90,
+            "draggable": false,
+            "isStage": false,
+            "layerOrder": 1,
+            "lists": {},
+            "name": "Sprite1",
+            "rotationStyle": "all around",
+            "size": 100,
+            "sounds": [
+                {
+                    "assetId": "83c36d806dc92327b9e7049a565c6bff",
+                    "dataFormat": "wav",
+                    "format": "",
+                    "md5ext": "83c36d806dc92327b9e7049a565c6bff.wav",
+                    "name": "Meow",
+                    "rate": 44100,
+                    "sampleCount": 37376
+                }
+            ],
+            "variables": {},
+            "visible": true,
+            "volume": 100,
+            "x": 0,
+            "y": 0
+        }
+    ]
+};
+var project = defaultProject;
